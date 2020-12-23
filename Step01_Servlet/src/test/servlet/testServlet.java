0567658d8aa50385c0755724ba4e5d707d068365
kimhongju <kinhoz9321@gmail.com>
2020-12-23 18:05:55 +0900

    Step01_Servelet Servelet form 저장

---
+++ Step01_Servlet/src/test/servlet/testServlet.java
@@ -0,0 +1,13 @@
+package test.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/showtime")
+public class testServlet extends HttpServlet{
+	
+	protected void service(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException ,java.io.IOException {
+		
+	};
+	
+}
