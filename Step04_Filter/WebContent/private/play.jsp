f7f3270530ae6274e2bace88023709add742f2fa
kimhongju <kinhoz9321@gmail.com>
2020-12-29 16:49:48 +0900

    Step04_Filter 로그인 필터 구현 추가함

--- Step04_Filter/WebContent/private/play.jsp
+++ Step04_Filter/WebContent/private/play.jsp
@@ -10,11 +10,6 @@
 	<%
 	//로그인된 아이디 읽어오기
 	String id=(String)session.getAttribute("id");
-	if(id==null){//만일 로그인을 하지 않았으면
-		//로그인 폼으로 강제 리다일렉트 시킨다. (로그인이 필요합니다.) * 중요! *
-		String cPath=request.getContextPath();
-		response.sendRedirect(cPath+"/test/loginform.jsp");
-	}
 	%>
 	<p><strong><%=id %></strong> 님 신나게 놀아 보아요!</p>
 	<a href="../index.jsp">인덱스로 가기</a>
---
+++ Step04_Filter/src/test/filter/LoginFilter.java
@@ -0,0 +1,55 @@
+package test.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginFilter implements Filter{
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//필터가 동작되면 호출되는 메소드
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException{
+		//1. 로그인된 클라이언트인지 확인한다. (부모타입이라 request.getsession 을 찾을 수 없으니 캐스팅 해야한다.)
+		
+		//부모 type을 자식 type으로 casting!
+		HttpServletRequest req=(HttpServletRequest)request;
+		//자식 type을 이용해서 HttpSession 객체의 참조값을 얻어낸다.
+		HttpSession session=req.getSession();
+		//로그인된 아이디가 있는지 읽어와 본다.
+		String id=(String)session.getAttribute("id");
+		//만일 로그인 된 상태라면
+		
+		if(id!=null) {
+			//2. 만일 로그인을 했으면 관여하지 않고 요청의 흐름을 이어간다.
+			chain.doFilter(request, response);
+		}else {
+			//3. 로그인을 하지 않았으면 로그인 폼으로 이동할 수 있도록 리다일렉트 응답을 준다.
+			String cPath=req.getContextPath();
+			//ServletResponse type을 HttpServletResponse type 으로 casting
+			HttpServletResponse res=(HttpServletResponse)response;
+			res.sendRedirect(cPath+"/test/loginform.jsp");
+		}
+	}
+		
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
