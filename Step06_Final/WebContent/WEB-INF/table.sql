384b3bef630f62a174a4c40634fa133c07c68ebd
kimhongju <kinhoz9321@gmail.com>
2021-01-07 10:17:03 +0900

    Step06_Final table.sql sql 작성 메모 추가

--- Step06_Final/WebContent/WEB-INF/table.sql
+++ Step06_Final/WebContent/WEB-INF/table.sql
@@ -8,3 +8,16 @@ CREATE TABLE users(
 	profile VARCHAR2(100), -- 프로필 이미지 경로를 저장할 칼럼
 	regdate DATE -- 가입일
 );
+
+-- 게시글을 저장할 테이블
+CREATE TABLE board_cafe(
+	num NUMBER PRIMARY KEY, --글번호
+	writer VARCHAR2(100) NOT NULL, --작성자 (로그인된 아이디)
+	title VARCHAR2(100) NOT NULL, --제목
+	content CLOB, --글 내용
+	viewCount NUMBER, --조회수
+	regdate DATE --글 작성일
+);
+
+-- 게시글의 번호를 얻어낼 시퀀스
+CREATE SEQUENCE board_cafe_seq;
