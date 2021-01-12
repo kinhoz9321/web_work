d31dcc78f8d9ada11bea2276806c9d43a34c54d6
kimhongju <kinhoz9321@gmail.com>
2021-01-12 10:56:55 +0900

    Step06_Final 검색기능 구현을 위한 FileDao 수정함

--- Step06_Final/WebContent/file/list.jsp
+++ Step06_Final/WebContent/file/list.jsp
@@ -17,15 +17,61 @@
 	int startRowNum=1+(pageNum-1)*PAGE_ROW_COUNT;
 	int endRowNum=pageNum*PAGE_ROW_COUNT;
 	
+	/*
+		[검색 키워드에 관련된 처리]
+		- 검색 키워드가 파라미터로 넘어올 수도 있고, 안 넘어 올수도 있다.
+	*/
+	String keyword=request.getParameter("keyword");
+	String condition=request.getParameter("condition");
+	//만일 키워드가 넘어오지 않는다면
+	if(keyword==null){
+		//키워드와 검색 조건에 빈 문자열을 넣어준다.
+		//클라이언트 웹브라우저에 출력할 때 "null"을 출력되지 않게 하기 이해서
+		keyword="";//키워드
+		condition="";//검색조건
+	}
+	
+	
 	FileDto dto=new FileDto();
 	dto.setStartRowNum(startRowNum);
 	dto.setEndRowNum(endRowNum);
 	
-	List<FileDto> list=FileDao.getInstance().getList(dto);
+	//ArrayList 객체의 참조값을 담을 지역변수를 미리 만든다.
+	List<FileDto> list=null;
 	
+	//전체 row의 갯수를 담을 지역변수를 미리 만든다.
+	int totalRow=0;
+	
+	//만일 검색 키워드가 넘어온다면 (3개의 블럭) 메소드 총 6개 추가하기
+	if(!keyword.equals("")){//오늘의 로직처리 부분!(키워드가 있을때 어떻게 달라지는지!)
+		//검색 조건이 무엇이냐에 따라 분기하기
+		if(condition.equals("title_filename")){//제목+파일명 검색인 경우
+			//검색 키워드를 FileDto에 담아서 전달한다.
+			dto.setTitle(keyword);
+			dto.setOrgFileName(keyword);
+			
+		}else if(condition.equals("title")){//제목 검색인 경우
+			dto.setTitle(keyword);
+		
+		}else if(condition.equals("writer")){//작성자 검색인 경우
+			dto.setWriter(keyword);
+			
+		}//다른 검색 조건을 추가하고 싶다면 아래에 else if() 를 계속 추가하면 된다.
+	}else{//검색 키워드가 넘어오지 않는다면 (1개의 블럭)
+		//키워드가 없을때 호출하는 메소드를 이용해서 파일 목록을 얻어온다.
+		list=FileDao.getInstance().getList(dto);
+		//키워드가 없을때 호출하는 메소드를 이용해서 전체 row의 갯수를 얻어온다.
+		totalRow=FileDao.getInstance().getCount();
+	}
+	/*
+	각각의 블럭에서 하는 일
+	- 검색 조건에 맞는 글 목록과 글의 갯수 얻어오기
+	
+	[팁]
+	컨트롤 누른 상태에서 filedao 클릭하면 바로 filedao로 넘어갈 수 있다.
+	*/
 	int startPageNum=1+((pageNum-1)/PAGE_DISPLAY_COUNT)*PAGE_DISPLAY_COUNT;
 	int endPageNum=startPageNum+PAGE_DISPLAY_COUNT-1;
-	int totalRow=FileDao.getInstance().getCount();
 	int totalPageCount=(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
 	if(endPageNum > totalPageCount){
 		endPageNum=totalPageCount;
@@ -121,6 +167,26 @@ ctrl+f 해서 replace 하기
 			<%} %>
 		</ul>
 	</nav>
+	<!-- filedao dynamic query 사용해야함. -->
+	<form class="row g-3 justify-content-center" action="list.jsp" method="get">
+		<div class="col-auto">
+			<label for="condition">검색조건</label>
+		</div>
+		<div class="col-auto">
+			<select class="form-control" style="width:200px" name="condition" id="condition">
+				<option value="title_filename">제목+파일명</option>
+				<option value="title">제목</option>
+				<option value="writer">작성자</option>
+			</select>
+		</div>
+		<div class="col-auto">
+			<input class="form-control" style="width:500px" type="text" name="keyword" placeholder="검색어"/>
+		</div>
+		<div class="col-auto">
+			<button class="btn btn-success" type="submit">검색</button>
+		</div>
+	</form>
+	<br />
 	<button class="btn btn-warning">
 		<a href="${pageContext.request.contextPath}/index.jsp" style="color:white">인덱스로 돌아가기</a>
 	</button>
--- Step06_Final/src/test/file/dao/FileDao.java
+++ Step06_Final/src/test/file/dao/FileDao.java
@@ -24,6 +24,149 @@ public class FileDao {
 		return dao;
 	}
 	
+	//검색조건 제목+파일명 검색인 경우 row 갯수
+	public int getCountTF(FileDto dto) {
+		int count=0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성
+			String sql = "SELECT NVL(MAX(ROWNUM),0) AS num"
+					+ " FROM board_file"
+					+ " WHERE title LIKE '%'||?||'%'"
+					+ " OR orgFileName LIKE '%'||?||'%'";//'%kim%'파일명 중에 kim이라는 문자열이 포함만 되어 있으면 (true가 되어) 셀렉트 됨.
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.(키워드는 같지만 다른 필드에 있는 내용을 불러와서 집어넣음)
+			//이렇게 하면 스프링 배울때 편하다.
+			/*
+			 * [title 검색 키워드가 "kim" 이라고 가정하면] 
+			 * 
+			 * 값 바인딩 전
+			 * 1. title LIKE '%' || ? || '%'
+			 * 
+			 * 값 바인딩 후
+			 * 2. title LIKE '%' || 'kim' || '%'
+			 * 
+			 * 연결연산 후 아래와 같은 SELECT 문이 구성된다.
+			 * 3. title LIKE '%kim%'
+			 * 
+			 * 따라서 제목에 kim 이라는 문자열이 포함된 row가 SELECT 된다.
+			 */
+			pstmt.setString(1, dto.getTitle());
+			pstmt.setString(2, dto.getOrgFileName());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			if (rs.next()) {
+				count=rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return count;
+	}
+	
+	//검색조건 제목 검색인 경우 row 갯수
+	public int getCountT(FileDto dto) {
+		int count=0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성
+			String sql = "SELECT NVL(MAX(ROWNUM),0) AS num"
+					+ " FROM board_file"
+					+ " WHERE title LIKE '%'||?||'%'";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.(키워드는 같지만 다른 필드에 있는 내용을 불러와서 집어넣음)
+			pstmt.setString(1, dto.getTitle());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			if (rs.next()) {
+				count=rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return count;
+	}
+	
+	//검색조건 작성자 검색인 경우 row 갯수
+	public int getCountW(FileDto dto) {
+		int count=0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성
+			String sql = "SELECT NVL(MAX(ROWNUM),0) AS num"
+					+ " FROM board_file"
+					+ " WHERE writer LIKE '%'||?||'%'";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.(키워드는 같지만 다른 필드에 있는 내용을 불러와서 집어넣음)
+			pstmt.setString(1, dto.getWriter());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			if (rs.next()) {
+				count=rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return count;
+	}
+	
 	//전체 row의 갯수를 리턴하는 메소드
 		public int getCount() {
 			int count=0;
@@ -146,6 +289,179 @@ public class FileDao {
 			}
 		}
 		return dto;
+	}
+	
+	//제목 파일명 검색인 경우에 파일 목록 리턴
+	public List<FileDto> getListTF(FileDto dto){
+		List<FileDto> list=new ArrayList<FileDto>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성
+			String sql = "SELECT *" + 
+					"	FROM" + 
+					"		(SELECT result1.*, ROWNUM AS rnum" + 
+					"		FROM" + 
+					"			(SELECT num, writer, title, orgFileName, fileSize, regdate" + 
+					"			FROM board_file"+ 
+					"         	WHERE title LIKE '%'||?||'%'"+ 
+					"			OR orgFileName LIKE '%'||?||'%'" + 
+					"			ORDER BY num DESC) result1)" + 
+					"	WHERE rnum BETWEEN ? AND ?";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.
+			pstmt.setString(1, dto.getTitle());
+			pstmt.setString(2, dto.getOrgFileName());
+			pstmt.setInt(3, dto.getStartRowNum());
+			pstmt.setInt(4, dto.getEndRowNum());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			while (rs.next()) {
+				FileDto dto1=new FileDto();
+				dto1.setNum(rs.getInt("num"));
+				dto1.setWriter(rs.getString("writer"));
+				dto1.setTitle(rs.getString("title"));
+				dto1.setOrgFileName(rs.getString("orgFileName"));
+				dto1.setFileSize(rs.getLong("fileSize"));
+				dto1.setRegdate(rs.getString("regdate"));
+				list.add(dto1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return list;
+	}
+	
+	//제목 검색인 경우에 파일 목록 리턴
+	public List<FileDto> getListT(FileDto dto){
+		List<FileDto> list=new ArrayList<FileDto>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성
+			String sql = "SELECT *" + 
+					"	FROM" + 
+					"		(SELECT result1.*, ROWNUM AS rnum" + 
+					"		FROM" + 
+					"			(SELECT num, writer, title, orgFileName, fileSize, regdate" + 
+					"			FROM board_file"+ 
+					"         	WHERE title LIKE '%'||?||'%'"+ 
+					"			ORDER BY num DESC) result1)" + 
+					"	WHERE rnum BETWEEN ? AND ?";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.
+			pstmt.setString(1, dto.getTitle());
+			pstmt.setInt(2, dto.getStartRowNum());
+			pstmt.setInt(3, dto.getEndRowNum());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			while (rs.next()) {
+				FileDto dto1=new FileDto();
+				dto1.setNum(rs.getInt("num"));
+				dto1.setWriter(rs.getString("writer"));
+				dto1.setTitle(rs.getString("title"));
+				dto1.setOrgFileName(rs.getString("orgFileName"));
+				dto1.setFileSize(rs.getLong("fileSize"));
+				dto1.setRegdate(rs.getString("regdate"));
+				list.add(dto1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return list;
+	}
+	
+	//작성자 검색인 경우에 파일 목록 리턴
+	public List<FileDto> getListW(FileDto dto){
+		List<FileDto> list=new ArrayList<FileDto>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성
+			String sql = "SELECT *" + 
+					"	FROM" + 
+					"		(SELECT result1.*, ROWNUM AS rnum" + 
+					"		FROM" + 
+					"			(SELECT num, writer, title, orgFileName, fileSize, regdate" + 
+					"			FROM board_file"+ 
+					"         	WHERE writer LIKE '%'||?||'%'"+ 
+					"			ORDER BY num DESC) result1)" + 
+					"	WHERE rnum BETWEEN ? AND ?";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setInt(2, dto.getStartRowNum());
+			pstmt.setInt(3, dto.getEndRowNum());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			while (rs.next()) {
+				FileDto dto1=new FileDto();
+				dto1.setNum(rs.getInt("num"));
+				dto1.setWriter(rs.getString("writer"));
+				dto1.setTitle(rs.getString("title"));
+				dto1.setOrgFileName(rs.getString("orgFileName"));
+				dto1.setFileSize(rs.getLong("fileSize"));
+				dto1.setRegdate(rs.getString("regdate"));
+				list.add(dto1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return list;
 	}
 	
 	//업로드 된 파일 목록을 리턴하는 메소드
